cf9cb76679804c36813244dcc02e59ed7165231e
danila4211 <danila421@bk.ru>
2023-12-12 10:47:32 +0400

    Добавьте файлы проекта.

---
+++ WebProject/Views/Home/All_Items.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebProject.Views.Home
+{
+    public class All_ItemsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
